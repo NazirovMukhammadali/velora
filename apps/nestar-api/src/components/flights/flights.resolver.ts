5dba4b55e595b67aee392cfd1d17296fdf4eab4e
Ali <nazirovali12345@gmail.com>
2026-04-20 23:40:13 +0900

    feat(flights): restrict createFlight to ADMIN role

--- apps/nestar-api/src/components/flights/flights.resolver.ts
+++ apps/nestar-api/src/components/flights/flights.resolver.ts
@@ -1,21 +1,25 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
 import { Flight, Flights } from '../../libs/dto/flight/flight';
 import { FlightsInquiry, FlightInput } from '../../libs/dto/flight/flight.input';
 import { FlightsService } from './flights.service';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { MemberType } from '../../libs/enums/member.enum';
 
 @Resolver()
 export class FlightsResolver {
     constructor(private readonly flightsService: FlightsService) { }
 
+    @Roles(MemberType.ADMIN)
+    @UseGuards(RolesGuard)
     @Mutation(() => Flight)
     public async createFlight(@Args('input') input: FlightInput): Promise<Flight> {
-        console.log('Mutation: createFlight');
         return await this.flightsService.createFlight(input);
     }
 
     @Query(() => Flights)
     public async getFlights(@Args('input') input: FlightsInquiry): Promise<Flights> {
-        console.log('Query: getFlights');
         return await this.flightsService.getFlights(input);
     }
 }
